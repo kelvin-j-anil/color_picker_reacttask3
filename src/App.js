bfb8dc501ea9a8499c2d5b10a871c876876a2726
KELVIN J ANIL <kelvinjanil@gmail.com>
2023-10-14 18:19:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ColorPicker } from './Components/ColorPicker';
+
+function App() {
+  return (
+    <ColorPicker/>
+  );
+}
+
+export default App;
